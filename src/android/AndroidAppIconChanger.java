517984020a1d21f453f6b7455f6d5912e3165d53
biswas123 <biswas.khayargoli@gmail.com>
2019-09-03 17:23:24 +0545

    First commit:

--- src/android/AndroidAppIconChanger.java
+++ src/android/AndroidAppIconChanger.java
@@ -19,23 +19,21 @@ public class AndroidAppIconChanger extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("changeAppIcon")) {
             String package_name = args.getString(0);
-			String icon_name = args.getString(1);
-            this.coolMethod(package_name, icon_name, callbackContext);
+			String alias_name = args.getString(1);
+            this.changeAppIcon(package_name, alias_name, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String package_name, String icon_name, CallbackContext callbackContext) {
-        if (package_name != null && package_name.length() > 0 && icon_name != null && icon_name.length() > 0) {            
+    private void changeAppIcon(String package_name, String alias_name, CallbackContext callbackContext) {
+        if (package_name != null && package_name.length() > 0 && alias_name != null && alias_name.length() > 0) {            
 			try {
 	
-				this.cordova.getActivity().getPackageManager().setComponentEnabledSetting(
-                    this.cordova.getActivity().getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+				this.cordova.getActivity().getPackageManager().setComponentEnabledSetting(this.cordova.getActivity().getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
 
-				this.cordova.getActivity().getPackageManager().setComponentEnabledSetting(
-                    new ComponentName(this.cordova.getActivity().getApplicationContext(),
-                            package_name + "." + icon_name),
+				this.cordova.getActivity().getPackageManager().setComponentEnabledSetting(new ComponentName(this.cordova.getActivity().getApplicationContext(),
+                            package_name + "." + alias_name),
                     PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                     PackageManager.DONT_KILL_APP);
 				callbackContext.success("OK");
